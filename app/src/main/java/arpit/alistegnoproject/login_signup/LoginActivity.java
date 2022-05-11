75273b9229cd8d7c0341e7cde9b1660433928c16
arpitparekh <arpitparekh9@gmail.com>
2022-05-11 20:17:14 +0530

    designing

---
+++ app/src/main/java/arpit/alistegnoproject/login_signup/LoginActivity.java
@@ -0,0 +1,16 @@
+package arpit.alistegnoproject.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import arpit.alistegnoproject.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
